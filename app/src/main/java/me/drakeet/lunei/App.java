ea4533163fbd0431e6e8348f17287a476e105a5f
drakeet <drakeet.me@gmail.com>
2016-01-23 23:32:01 +0800

    Add AppConfig.java to git ignore.

--- app/src/main/java/me/drakeet/lunei/App.java
+++ app/src/main/java/me/drakeet/lunei/App.java
@@ -3,6 +3,8 @@ package me.drakeet.lunei;
 import android.app.Application;
 import com.avos.avoscloud.AVOSCloud;
 
+import static me.drakeet.lunei.AppConfig.*;
+
 /**
  * Created by drakeet(http://drakeet.me)
  * Date: 16/1/23 23:28
@@ -11,6 +13,6 @@ public class App extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
-        AVOSCloud.initialize(this, AppConfig.APP_KEY, AppConfig.APP_SECRET);
+        AVOSCloud.initialize(this, APP_KEY, APP_SECRET);
     }
 }
